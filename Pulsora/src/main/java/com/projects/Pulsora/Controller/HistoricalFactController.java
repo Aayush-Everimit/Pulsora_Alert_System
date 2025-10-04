70ff5e6679d688afaea7cad22bb6cfab9fc535a5
Aayush-Everimit <Aayush_2006@outlook.com>
2025-10-04 18:07:52 +0530

    Update

--- Pulsora/src/main/java/com/projects/Pulsora/Controller/HistoricalFactController.java
+++ Pulsora/src/main/java/com/projects/Pulsora/Controller/HistoricalFactController.java
@@ -3,5 +3,7 @@ package com.projects.Pulsora.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HistoricalFactController {
+public class HistoricalFactController
+{
+    System.out.println("Hello");
 }
